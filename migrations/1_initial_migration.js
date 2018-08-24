823738f860768e0bc3880e75ca8e25a3a3780015
Hao Jin <jh@highsharp.com>
2018-08-24 10:22:14 +0800

    migration js scripts can have network, addresses parameters other than the deployer

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, addresses) {
+  //console.log(this);
+  // if (network == 'privatenet') {
+  //   console.log("unlocking accounts");
+  //   web3.personal.unlockAccount(addresses[0], "highsharp", 36000);
+  //   web3.personal.unlockAccount(addresses[1], "highsharp", 36000);
+  // }
   deployer.deploy(Migrations);
+  console.log('>> Deployed migration');
+ 
 };// 
