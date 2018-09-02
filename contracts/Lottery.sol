7be8dda59cc7632a9be85b3f97fa105f319f3b19
Hao Jin <jh@highsharp.com>
2018-09-02 23:32:51 +0800

    1. check and store lottery stubs 2. add time to a lottery data

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,16 +2,27 @@ pragma solidity ^0.4.24;
 
 contract Lottery
 {
+    event VerifyLottery(bytes lottery, bytes sig, bytes winningData, address sender);
+    event RedeemedLotttery(bytes lottery, uint64 issuingTime, uint256 faceValue, address issuer, address winner);
     struct Escrow {
         uint deposite;
     }
+    
+    struct Stub {
+        bool stored;
+        bytes32 h1;// hash of random string1
+        bytes32 h2;// hash of random string1 + random string2
+    }
 
     mapping(address => Escrow) accounts;
+    mapping(address => Stub[10]) stubsMapping; // lottery issuer to lottery stub
+    mapping(address => uint8) stubsIndex; // each index store where the stub is stored
+    
     uint8 power = 18;
-    uint256 faceValue = 10 finney;
+    uint256 faceValue = 10 finney; // unit is wei
 
     /// Get escrow by account address
-    function getEscrow(address account) public view returns (uint deposite)
+    function getEscrow(address account) public view returns (uint256 deposite)
     {
         Escrow storage esc = accounts[account];
         deposite = esc.deposite;
@@ -26,23 +37,35 @@ contract Lottery
     }
 
     function verify(bytes lottery, bytes signature, bytes winningData) public returns (bool success){
-        address addr = verifySig(signature, lottery);
-        require (addr != 0x00, "Signature verification failed");
-        (bytes1 ver, bytes memory rs2, bytes32 hashRs1, address dest) = splitLottery(lottery); 
-        if (dest == 0x00) {
+        emit VerifyLottery(lottery, signature, winningData, msg.sender);
+        address issuer = verifySig(signature, lottery);
+        require (issuer != 0x00, "Signature verification failed");
+        (bytes1 ver, bytes memory rs2, bytes32 hashRs1, address dest, uint64 time) = splitLottery(lottery); 
+         if (dest == 0x00) {
             dest = msg.sender;
         }
+
+        // bytes32 hrs2 = getHash(rs2);
+        bytes32 hashRs1Rs2 = constructHashRs1Rs2(winningData, rs2);
+        bool found = checkStubs(hashRs1, hashRs1Rs2, issuer); //prevent replay attack
+        require(!found, "The lottery has been redeemed.");
+       
         require(verifyRs1Hash(winningData, hashRs1), "Hash of the random string 1 does not match.");
-        if (verifyLottery(uint8(ver), winningData, rs2)) {
-            success = transfer(addr, dest);
+       
+        if (verifyLottery(uint8(ver), hashRs1Rs2, rs2)) {
+            success = transfer(issuer, dest);
+            if (success) {
+                storeStub(issuer, hashRs1, hashRs1Rs2);
+                emit RedeemedLotttery(lottery, time, faceValue, issuer, dest);
+            }
         } else {
             success = false;
         }
     }
     
     function transfer(address source, address dest) public returns (bool ret){
-        Escrow storage esc = accounts[source];
-        if (esc.deposite >= faceValue) {
+         Escrow storage esc = accounts[source];
+         if (esc.deposite >= faceValue) {
             esc.deposite -= faceValue;
             dest.transfer(faceValue);
             ret = true;
@@ -63,17 +86,47 @@ contract Lottery
     function getHash(bytes data) internal pure returns (bytes32 h) {
         h = keccak256(data);
     }
-    
-    // sha256 does not work in production environment, though it works in evm.
-    // function getSHA256(bytes data) public pure returns (bytes32 h) {
-    //     h = sha256(data);
-    // }
-
     /// Verify if a lottery wins and tranfer its face value to the winner
-    function verifyLottery(uint8 ver, bytes rs1, bytes rs2) internal view returns (bool)
+    function verifyLottery(uint8 ver, bytes32 hashRs1Rs2, bytes rs2) public view returns (bool)
     {
         require(ver == 0, "Version must be 0");
-       // require(sha256(rs1) == hashRS1, "The random string 1 or its hash supplied is incorrect.");
+        bytes32 hashRs2 = getHash(rs2);
+
+        return verifyXOR(hashRs1Rs2, hashRs2, power);
+    }
+    
+    function checkStubs(bytes32 hashRs1, bytes32 hashRs1Rs2, address addr) internal view returns (bool found){
+        Stub[10] storage stubs = stubsMapping[addr];
+  
+        bool foundEmpty = false;
+        found = false;
+        for (uint8 i = 0; i < stubs.length; i++) {
+            Stub storage stub = stubs[i];
+            if (!stub.stored) {
+                foundEmpty = true; 
+                //stubIndex = i;
+                break;
+            }
+            found = stub.h1 == hashRs1 && stub.h2 == hashRs1Rs2;
+            if (found) {
+                break;
+            }
+        }
+
+    }
+    
+    function storeStub(address issuer, bytes32 hashRs1, bytes32 hashRs1Rs2) internal  {
+        uint8 stubIndex = stubsIndex[issuer];
+        Stub storage stub = stubsMapping[issuer][stubIndex];
+        stub.stored = true;
+        stub.h1 = hashRs1;
+        stub.h2 = hashRs1Rs2;
+        stubIndex = (stubIndex + 1) % 10;
+        stubsIndex[issuer] = stubIndex;
+    }
+    
+    function constructHashRs1Rs2(bytes rs1, bytes rs2) internal pure returns (bytes32 hashRs1Rs2)
+    {
         bytes memory rs1Rs2 = new bytes(rs1.length + rs2.length);
         for (uint8 i = 0; i < rs1.length; i++) {
             rs1Rs2[i] = rs1[i];
@@ -82,11 +135,7 @@ contract Lottery
         for ( i = 0; i < rs2.length; i++) {
             rs1Rs2[i + offset] = rs2[i];
         }
-
-        bytes32 hashRs1Rs2 = getHash(rs1Rs2);
-        bytes32 hashRs2 = getHash(rs2);
-
-        return verifyXOR(hashRs1Rs2, hashRs2, power);
+        hashRs1Rs2= getHash(rs1Rs2);
     }
 
     function verifyXOR(bytes32 hRs1Rs2, bytes32 hRs2, uint8 n) internal pure returns (bool ret)
@@ -144,8 +193,8 @@ contract Lottery
         return (v, r, s);
     }
 
-    function splitLottery(bytes memory lottery) internal pure 
-    returns (bytes1 ver, bytes rs2, bytes32 hashRs1, address addr)
+    function splitLottery(bytes memory lottery) public pure 
+    returns (bytes1 ver, bytes rs2, bytes32 hashRs1, address addr, uint64 time)
     {
         ver = lottery[0];
         require(ver == 0, "Only version 0 is supported.");
@@ -161,13 +210,10 @@ contract Lottery
            hashRs1 := mload(add(lottery, add(32, offset)))
            offset := add(offset, 52)
            addr := mload(add(lottery, offset))
+           offset := add(offset, 8)
+           time := mload(add(lottery, offset))
         }
         
         
     }
-
-
-    /// Store the stub of the winning lottery
-
-    /// Check if the winning lottery has been redeemed once.
 }
