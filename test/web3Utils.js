098945916683b44e417fc56f6320ee9f4c6d1aae
Hao Jin <jh@highsharp.com>
2018-10-29 18:01:35 +0800

    use env variable web3Addr to replace the fixed url

--- test/web3Utils.js
+++ test/web3Utils.js
@@ -1,9 +1,12 @@
 const Web3 = require('web3')
+
+const addr = process.env.web3Addr? process.env.web3Addr: "http://localhost:8565";
+console.log("connecting to", addr);
 if (typeof web3 !== 'undefined') {
     var web3 = new Web3(web3.currentProvider);
    } else {
     // set the provider you want from Web3.providers
-    var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8565"));
+    var web3 = new Web3(new Web3.providers.HttpProvider(addr));
 }
 
 module.exports = web3;
